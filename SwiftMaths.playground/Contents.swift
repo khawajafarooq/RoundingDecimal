343c683ea5aaae4c15e847942741f2bb0f213ac4
Khawaja Farooq <kfarooqa@gmail.com>
2017-10-10 19:52:07 +0300

    fluent function name

--- SwiftMaths.playground/Contents.swift
+++ SwiftMaths.playground/Contents.swift
@@ -2,16 +2,16 @@ import Foundation
 
 extension Double {
     
-    mutating func roundToPlaces(places: Int) -> Double {
+    mutating func round(to places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return Darwin.round(self * divisor) / divisor
     }
 }
 
 var decimal = 10035.11459
-print(decimal.roundToPlaces(places: 2))
-print(decimal.roundToPlaces(places: 3))
-print(decimal.roundToPlaces(places: 4))
+print(decimal.round(to: 2))
+print(decimal.round(to: 3))
+print(decimal.round(to: 4))
 
 
 
